5f374740941ed7de6620d4a2e2b61dfefc3a9d6e
berkkarasu <91190579+berkkarasu@users.noreply.github.com>
2022-12-15 12:29:45 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) throws IOException{
-                List<List<String>> list = readData();
+                List<List<String>> list = readValues();
                 System.out.println(list.get(0));
 		List<Integer> valueList = new ArrayList<Integer>();
 		List<Integer> weightList = new ArrayList<Integer>();
@@ -17,14 +15,60 @@ public class Main {
 			valueList.add(Integer.parseInt(list.get(i).get(4))); 
 			weightList.add(Integer.parseInt(list.get(i).get(5)));
 			}
+		List<List<String>> list1 = readSequential();
+		List<ArrayList<Double>> sequential_data = new ArrayList<ArrayList<Double>>();
+		for (int i=1;i<list1.size();i++){
+			ArrayList<Double> row = new ArrayList<>();
+			for (int j=1;j<list1.get(0).size();j++){
+				row.add(Double.parseDouble(list1.get(i).get(j)));
+			}
+			sequential_data.add(row);
+		}
+		System.out.println(sequential_data.get(0).get(1));
+		
 		
 
         }
-        public static List<List<String>> readData() throws IOException { 
+        public static List<List<String>> readValues() throws IOException { 
                 try
 		{
 			List< List<String> > data = new ArrayList<>();//list of lists to store data
 			String file = "term_project_value_data.csv";//file path
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			
+			//Reading until we run out of lines
+			String line = br.readLine();
+			while(line != null)
+			{
+				List<String> lineData = Arrays.asList(line.split(","));//splitting lines
+				data.add(lineData);
+				line = br.readLine();
+			}
+			
+			//printing the fetched data
+			for(List<String> list : data)
+			{
+				for(String str : list)
+					System.out.print(str + " ");
+				System.out.println();
+			}
+			br.close();
+                        return data;
+		}
+		catch(Exception e)
+		{
+			System.out.print(e);
+                        List< List<String> > data = new ArrayList<>();//list of lists to store data
+                        return data;
+		}
+                
+        }
+	public static List<List<String>> readSequential() throws IOException { 
+                try
+		{
+			List< List<String> > data = new ArrayList<>();//list of lists to store data
+			String file = "term_project_sequential_data.csv";//file path
 			FileReader fr = new FileReader(file);
 			BufferedReader br = new BufferedReader(fr);
 			
